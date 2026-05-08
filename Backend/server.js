34213d21b64261864b089bb69a586be012c230d8
shaloom <shaloombenjamin615@email.com>
2026-05-08 15:57:45 +0500

    Hello Ghaffar

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,24 @@
-console.log ("Hello") 
+// server.js
+
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+
+const app = express();
+
+const port = 3000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Server is running on port 3000");
+});
+
+// Start Server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
